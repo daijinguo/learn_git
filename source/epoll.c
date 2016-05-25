69dafbfef0b04caa9d3df1ad2824d63d5bf959e2
戴金国 <jinguo504@163.com>
2016-05-25 22:36:55 +0800

    在epoll代码上添加了几个create和close函数

--- source/epoll.c
+++ source/epoll.c
@@ -1,1 +1,12 @@
 #include "epoll.h"
+
+int epool_create() {
+	return 0;
+}
+
+void epoll_close( int epoll_id ) {
+	if( 0 == epoll_id )
+		return;
+	
+	// close the  epoll
+}
--- source/epoll.h
+++ source/epoll.h
@@ -1,6 +1,10 @@
 #ifndef _EPOLL_H_
 #define _EPOLL_H_
 
+int epoll_create();
+
+void epoll_close( int epoll_id );
+
 
 
 #endif
