4cf8a7d9845d44314a50c1d101198b8ea0132d11
daijinguo <jinguo504@163.com>
2016-05-26 09:30:58 +0800

    添加merge close实现函数

--- merge/merge.cpp
+++ merge/merge.cpp
@@ -1,2 +1,10 @@
 #include "merge.h"
 
+#include <stdio.h>
+
+void func_close(int code){
+	if( code <= 0 )
+		return;
+	
+	close( code );
+}
--- merge/merge.h
+++ merge/merge.h
@@ -1,4 +1,6 @@
 #ifndef _MERGE_H_
 #define _MERGE_H_
 
+void func_close(int code);
+
 #endif
