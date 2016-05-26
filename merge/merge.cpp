0f1f76724d5c0c43649b8ed8928f126436ab291b
daijinguo <jinguo504@163.com>
2016-05-26 09:27:24 +0800

    添加merge实现函数 open

--- merge/merge.cpp
+++ merge/merge.cpp
@@ -1,2 +1,10 @@
 #include "merge.h"
 
+#include <stdio.h>
+
+int func_open() {
+	int iCode = 0;
+	// ....
+	return iCode;
+}
+
--- merge/merge.h
+++ merge/merge.h
@@ -1,4 +1,6 @@
 #ifndef _MERGE_H_
 #define _MERGE_H_
 
+int func_open();
+
 #endif
